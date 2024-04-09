75426aa271b7c56bce74d6090c578f40fc746654
v4nn4 <romflorentz@gmail.com>
2024-04-09 08:37:09 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # glyphs-generator-rs
+
+This repository is a Rust port of [glyphs-generator](https://github.com/v4nn4/glyphs-generator). it compiles to WebAssembly and is used in this [demo website](https://github.com/pages/v4nn4/glyphs-generator).
+
+## Compile
+
+To install dependencies and compile the Rust package, run
+
+```bash
+cargo build
+```
+
+In order to compile to WebAssembly, run 
+
+```bash
+wasm-pack build --target web
+```
+
+More information on https://developer.mozilla.org/en-US/docs/WebAssembly/Rust_to_Wasm.
+
+## Run tests
+
+To run unit tests, run
+
+```bash
+cargo test
+```
+
+## Run examples
+
+To run examples, run
+
+```bash
+cargo run --example hello
+```
---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+mod generate;
+mod glyph;
+mod parameters;
+mod stroke;
+
+#[cfg(test)]
+mod tests;
+
+use generate::GlyphGenerator;
+use parameters::Parameters;
+use serde::{Deserialize, Serialize};
+use stroke::Stroke;
+use wasm_bindgen::prelude::*;
+
+use once_cell::sync::OnceCell;
+
+static GENERATOR: OnceCell<GlyphGenerator> = OnceCell::new();
+
+#[wasm_bindgen]
+pub fn initialize(parameters_json: String) {
+    let parameters: Parameters =
+        serde_json::from_str(&parameters_json).expect("Error deserializing parameters JSON");
+    let generator = GlyphGenerator::new(parameters);
+    GENERATOR.set(generator).ok();
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Computable {
+    strokes: Vec<Stroke>,
+}
+
+#[wasm_bindgen]
+pub fn compute(computable_json: String) -> Result<String, JsValue> {
+    if GENERATOR.get().is_none() {
+        // Returning an error if the JSON data is not loaded
+        return Err(JsValue::from_str("Paremters JSON data not loaded"));
+    }
+    let generator = GENERATOR.get().unwrap();
+    let computable: Computable =
+        serde_json::from_str(&computable_json).expect("Error deserializing computable JSON");
+    let strokes = computable.strokes;
+    let result = generator.generate(&strokes, &strokes[0]);
+    Ok(serde_json::to_string(&result).unwrap())
+}
